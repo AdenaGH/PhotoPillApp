c52371f448698e70150adf7100307d05823af466
jparkd07 <79125969+jparkd07@users.noreply.github.com>
2023-04-16 18:05:41 -0400

    demo update

--- PhotoPill/TableViewController.swift
+++ PhotoPill/TableViewController.swift
@@ -22,16 +22,16 @@ class TableViewController : UIViewController, UITableViewDelegate, UITableViewDa
     }
     
     func initList() {
-        let pill1 = Pill(name: "Chill Pill", description: "Taken for the user to chill out.", imageName: "pill1")
+        let pill1 = Pill(name: "Alosetron", description: "Purpose: Manage and treat severe IBS-D \nImprint: LS 701 \nStrength: 1mg \nShape: Round \nColor: Blue \nForm: Tablet", imageName: "pill1")
         pillList.append(pill1)
         
-        let pill2 = Pill(name: "Chill Pill", description: "Taken for the user to chill out.", imageName: "pill2")
+        let pill2 = Pill(name: "Alprazolam", description: "Purpose:Manage panic and anxiety disorder \nImprint: 031 logo \nStrength: 1 mg \nShape: Round \nColor: Blue \nForm: Tablet", imageName: "pill2")
         pillList.append(pill2)
         
-        let pill3 = Pill(name: "Chill Pill", description: "Taken for the user to chill out.", imageName: "pill3")
+        let pill3 = Pill(name: "Amitriptyline", description: "Purpose: Tricyclic antidepressant - used to treat depression in adults \nImprint: 12 28 \nStrength: 75 mg \nShape: Round \nColor: Light Blue \nForm: Tablet", imageName: "pill3")
         pillList.append(pill3)
         
-        let pill4 = Pill(name: "Chill Pill", description: "Taken for the user to chill out.", imageName: "pill4")
+        let pill4 = Pill(name: "Amoxapine", description: "Purpose: Tricyclic antidepressant - used to treat depression in adults \nImprint: DAN 100 5715  logo \nStrength: 100 mg \nShape: Round \nColor: Blue \nForm: Tablet", imageName: "pill4")
         pillList.append(pill4)
         
         
