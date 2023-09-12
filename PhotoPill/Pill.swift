26216da4045ab16f25274de27c343a11006e9f05
aninvalle3 <adena@gatech.edu>
2023-09-11 21:15:39 -0400

    Initial commit, holds demo app data for Dart.

--- PhotoPill/Pill.swift
+++ PhotoPill/Pill.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+class Pill {
+    var name : String!
+    var description : String!
+    var imageName : String!
+    
+    
+    public init (name: String, description: String, imageName: String) {
+        self.name = name
+        self.description = description
+        self.imageName = imageName
+    }
+}
+
+//test
