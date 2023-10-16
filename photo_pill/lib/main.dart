b471be4b134e2ed7b2e1eac33f6a0fe0994b057b
aninvalle3 <adena@gatech.edu>
2023-10-16 17:56:40 -0400

    Removed several bulky comments

--- photo_pill/lib/main.dart
+++ photo_pill/lib/main.dart
@@ -16,21 +16,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'PhotoPill',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
@@ -42,14 +27,6 @@ class MyApp extends StatelessWidget {
 class LandingPage extends StatefulWidget {
   const LandingPage({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   @override
   State<LandingPage> createState() => _LandingPageState();
@@ -65,32 +42,27 @@ class _LandingPageState extends State<LandingPage> {
 ];
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  
     return Scaffold(
       body: IndexedStack(
   index: _selectedIndex,
   children: _pages,
 ),
-      bottomNavigationBar: BottomNavigationBar (
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined),
-            label: "Home",),
-            BottomNavigationBarItem(
-            icon: Icon(Icons.add_chart_rounded),
-            label: "Data",),
-            BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: "Search",),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
+      // bottomNavigationBar: BottomNavigationBar (
+      //   items: const <BottomNavigationBarItem>[
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.home_outlined),
+      //       label: "Home",),
+      //       BottomNavigationBarItem(
+      //       icon: Icon(Icons.add_chart_rounded),
+      //       label: "Data",),
+      //       BottomNavigationBarItem(
+      //       icon: Icon(Icons.search),
+      //       label: "Search",),
+      //   ],
+      //   currentIndex: _selectedIndex,
+      //   onTap: _onItemTapped,
+      // ),
        // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
