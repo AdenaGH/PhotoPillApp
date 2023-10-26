bccf555f71cb1c80e03e5492ee91a1e244e4b700
HariKumaran12345 <96630748+HariKumaran12345@users.noreply.github.com>
2023-10-26 15:23:57 -0400

    asks for camera permission

--- photo_pill/lib/main.dart
+++ photo_pill/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:photo_pill/inputDrugDescription.dart';
 import 'package:photo_pill/inputPatientMed.dart';
 import 'package:photo_pill/myHomePage.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,50 +28,65 @@ class MyApp extends StatelessWidget {
 class LandingPage extends StatefulWidget {
   const LandingPage({super.key});
 
-
   @override
   State<LandingPage> createState() => _LandingPageState();
 }
 
 class _LandingPageState extends State<LandingPage> {
+  bool _isPermissionGranted = false;
+
+  late final Future<void> _future;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _future = _requestCameraPermission();
+  }
+
+  Future<void> _requestCameraPermission() async {
+    final status = await Permission.camera.request();
+    _isPermissionGranted = status == PermissionStatus.granted;
+  }
+
   late int _selectedIndex = 0;
   static const _pages = [
-   MyHomePage(title: "PhotoPill"),
-   InputPatientMed(),
-   InputDrugDescription(),
-
-];
+    MyHomePage(title: "PhotoPill"),
+    InputPatientMed(),
+    InputDrugDescription(),
+  ];
   @override
   Widget build(BuildContext context) {
-  
     return Scaffold(
       body: IndexedStack(
-  index: _selectedIndex,
-  children: _pages,
-),
-      bottomNavigationBar: BottomNavigationBar (
+        index: _selectedIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home_outlined),
-            label: "Home",),
-            BottomNavigationBarItem(
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
             icon: Icon(Icons.add_chart_rounded),
-            label: "Data",),
-            BottomNavigationBarItem(
+            label: "Data",
+          ),
+          BottomNavigationBarItem(
             icon: Icon(Icons.search),
-            label: "Search",),
+            label: "Search",
+          ),
         ],
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
       ),
-       // This trailing comma makes auto-formatting nicer for build methods.
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
   void _onItemTapped(int index) {
-  setState(() {
-    _selectedIndex = index;
-  });
-}
-  
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 }
