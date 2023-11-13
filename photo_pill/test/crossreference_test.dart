48e1625fc82ca047bb36e0fd8d16e8c2509b58cc
Wang, Haijian <hbill6@gatech.edu>
2023-11-13 09:01:51 -0500

    Update fetch process and drug object
    static fetch function is now under search.dart file, see detail in doc
    id is included in drug class which acts the same as name attribute, both attributes share same rank

--- photo_pill/test/crossreference_test.dart
+++ photo_pill/test/crossreference_test.dart
@@ -13,15 +13,15 @@ void main() {
     expect(drug.size, "DEFAULT");
   });
   test('sample drug object creation test', () {
-    druglib.Drug drug = druglib.Drug("", "BLUE", "barrel shaped", "11 mm");
+    druglib.Drug drug = druglib.Drug("", "", "BLUE", "barrel shaped", "11 mm");
     expect(drug.name, "DEFAULT");
     expect(drug.color, "BLUE");
     expect(drug.shape, "barrel shaped");
     expect(drug.size, "11 mm");
   });
   test('sample drug rank test', () {
-    druglib.Drug drug = druglib.Drug("", "BLUE", "barrel shaped", "11 mm");
-    druglib.Drug target = druglib.Drug("", "BLUE", "barrel shaped", "11 mm");
+    druglib.Drug drug = druglib.Drug("", "", "BLUE", "barrel shaped", "11 mm");
+    druglib.Drug target = druglib.Drug("", "", "BLUE", "barrel shaped", "11 mm");
     drug.getRank(target);
     expect(drug.rank, 3);
   });
